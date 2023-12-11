2b0d81e434c0bb1eb729da63e4ce3cac4e49faba
maksim <maxixx666@gmail.com>
2023-12-11 22:36:17 +0300

    update

--- lux/assets/js/0617f994.35aea93e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunklux_docs=self.webpackChunklux_docs||[]).push([[223],{3769:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
---
+++ lux/assets/js/508dca98.c567141f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunklux_docs=self.webpackChunklux_docs||[]).push([[640],{3769:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
